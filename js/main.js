08fae5fa7acc496b2ca4ca9d4af615c1130d215b
vorvarinoo <n.styrman@yandex.ru>
2022-10-03 01:29:58 +0300

    Добавил функцию, которая возвращает случайное целое число из переданного диапазона.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+function getRandomNumber(min, max) {
+  if ((min < max) && ((min > -1) && (max > -1))) {
+    const resultat = Math.floor(Math.random() * ((max + 1) - min)) + min;
+    console.log(resultat);
+    // return resultat;
+  } else {
+    const resultat = 0 / 0;
+    return resultat;
+  }
 
+}
+
+getRandomNumber(1, 6);
