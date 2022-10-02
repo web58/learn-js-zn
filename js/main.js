da972a6ad7040b91067bf09b5869ca92cd441a2b
vorvarinoo <n.styrman@yandex.ru>
2022-10-03 01:33:27 +0300

    Добавил функцию для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 function getRandomNumber(min, max) {
   if ((min < max) && ((min > -1) && (max > -1))) {
     const resultat = Math.floor(Math.random() * ((max + 1) - min)) + min;
-    console.log(resultat);
-    // return resultat;
+    return resultat;
   } else {
     const resultat = 0 / 0;
     return resultat;
@@ -11,3 +10,17 @@ function getRandomNumber(min, max) {
 }
 
 getRandomNumber(1, 6);
+
+
+function lengthString(inputText, maxlength) {
+
+  if (inputText.length > maxlength) {
+    const resultat = false;
+    return resultat;
+  } else {
+    const resultat = true;
+    return resultat;
+  }
+}
+
+lengthString('Рыбный текст – это текст, выполняющий исключительно утилитарную функцию', 71);
