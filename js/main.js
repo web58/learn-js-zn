3efcd7c69e429704825d7e0b052d6f9b714341dc
vorvarinoo <87990429+vorvarinoo@users.noreply.github.com>
2022-10-09 04:48:56 +0300

    Написал функции по генерации данных и добавления их в массив

--- js/main.js
+++ js/main.js
@@ -17,3 +17,47 @@ function checkLengthString(inputText, maxlength) {
 }
 
 checkLengthString('Рыбный текст – это текст, выполняющий исключительно утилитарную функцию', 71);
+
+
+// Функции генерации поста
+const PHOTO_DESCRIPTION = [
+  'Однако ее карьера могла не быть такой блестящей, если бы актриса не получила культовую роль Китнисс Эвердин, для которой ее посчитали слишком красивой.',
+  'КС, Х — тут мы имеем дело с женщинами, готовыми совершать спонтанные поступки.',
+  'Ну и самый неуклюжий способ погибнуть от огня в джунглях — сунуться в огненные ворота в храме, не обезвредив их предварительно.',
+  'Она боится влюбляться, потому что погружается в отношения с полной отдачей.',
+];
+
+function creatValue(value) {
+  const mySet = new Set();
+  let newArray = [];
+  for (let i = 0; newArray.length < value; i++) {
+    newArray = Array.from(mySet.add(getRandomNumber(0, value)));
+  }
+  return newArray;
+}
+
+function getRandomArrayElement(e) {
+  return e[getRandomNumber(0, e.length - 1)];
+}
+
+const arrayId = creatValue(25);
+const arrayUrl = creatValue(25);
+
+function creatObj(e) {
+  return {
+    id: arrayId[e],
+    url: `photos/${arrayUrl[e]}.jpg`,
+    description: getRandomArrayElement(PHOTO_DESCRIPTION),
+    likes: getRandomNumber(15, 200),
+    comments: getRandomNumber(0, 200),
+  };
+
+
+}
+
+
+const creatArray = Array.from({
+  length: 25
+}, (e, index) => {
+  return creatObj(index);
+});
