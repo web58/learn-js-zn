8c8e020ff55cd21429ff622e59962e36a1080b79
vorvarinoo <87990429+vorvarinoo@users.noreply.github.com>
2022-10-30 05:58:16 +0300

    Добавил на форму отслеживание событий

--- js/main.js
+++ js/main.js
@@ -2,4 +2,31 @@ import {
   createPictures,
 } from './picture.js';
 
+import {
+  uploadPictureElement,
+  uploadFile,
+  closeModalCancel,
+  openModal,
+  closeModal,
+  validateFormDescription,
+  successPopup,
+  successBtn,
+} from './form.js';
+
 createPictures();
+
+uploadFile.addEventListener('change', openModal);
+closeModalCancel.addEventListener('click', closeModal);
+uploadPictureElement.addEventListener('submit', (evt) => {
+  if (!validateFormDescription.validate()) {
+    evt.preventDefault();
+  } else {
+    closeModal();
+    document.body.append(successPopup);
+    evt.preventDefault();
+  }
+});
+
+successBtn.addEventListener('click', () => {
+  document.body.querySelector('.success').remove();
+});
