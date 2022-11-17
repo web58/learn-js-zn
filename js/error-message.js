58bbcbfce518cb359cd42e9eead2c8458e643017
vorvarinoo <87990429+vorvarinoo@users.noreply.github.com>
2022-11-17 14:32:03 +0300

    Добавил закрытие Pop-up сообщений при клике на произвольную область экрана за пределами блока с сообщением.

--- js/error-message.js
+++ js/error-message.js
@@ -10,22 +10,31 @@ const errorNode = errorMessageTemplate.cloneNode(true);
 function errorMessage() {
   document.body.appendChild(errorNode);
   errorNode.addEventListener('click', closeError);
-  document.addEventListener( 'keydown', onEscKeydown );
+  document.addEventListener('keydown', onEscKeydown);
+  document.addEventListener('click', closeErrorClick);
 }
 
 
 function closeError(evt) {
-  if (evt.target.closest('.error') && !evt.target.closest('.error__button')) {
+  if (evt.target.closest('.error__inner') && !evt.target.closest('.error__button')) {
     return;
   }
   document.querySelector('.error').remove();
-  document.removeEventListener( 'keydown', onEscKeydown );
+  document.removeEventListener('keydown', onEscKeydown);
+  document.removeEventListener('click', closeErrorClick);
 }
 
 
 function onEscKeydown(evt) {
   if (isEscapeKey(evt)) {
     closeError(evt);
+  }
+}
+
+
+function closeErrorClick(evt) {
+  if (!evt.target.closest('.error__inner')) {
+    closeError();
   }
 }
 
--- js/success-message.js
+++ js/success-message.js
@@ -9,20 +9,29 @@ const successNode = successMessageTemplate.cloneNode(true);
 function successMessage() {
   document.body.appendChild(successNode);
   successNode.addEventListener('click', closeSuccess);
-  document.addEventListener( 'keydown', onEscKeydown );
+  document.addEventListener('keydown', onEscKeydown);
+  document.addEventListener( 'click', closeSuccessClick );
 }
 
 function closeSuccess(evt) {
-  if (evt.target.closest('.success') && !evt.target.closest('.success__button')) {
+  if (evt.target.closest('.success__inner') && !evt.target.closest('.success__button')) {
     return;
   }
   document.querySelector('.success').remove();
-  document.removeEventListener( 'keydown', onEscKeydown );
+  document.removeEventListener('keydown', onEscKeydown);
+  document.removeEventListener( 'click', closeSuccessClick );
 }
 
 function onEscKeydown(evt) {
   if (isEscapeKey(evt)) {
     closeSuccess(evt);
+  }
+}
+
+
+function closeSuccessClick(evt) {
+  if (!evt.target.closest('.success__inner')) {
+    closeSuccess();
   }
 }
 
