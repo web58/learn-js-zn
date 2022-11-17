e0796e7ad736823a41b747f5a25cecaa47cd0044
vorvarinoo <87990429+vorvarinoo@users.noreply.github.com>
2022-11-17 14:38:02 +0300

    Добавил fetchu заголовок запроса, добавил вывод pop-up сообщения об ошибке если метод fetch не получал данные json с указанного адреса

--- js/rendering-picture.js
+++ js/rendering-picture.js
@@ -2,11 +2,27 @@ import {
   creatingTemplatePictures,
 } from './creating-template-picture.js';
 
+import {
+  errorMessage,
+} from './error-message.js';
+
 function renderPictures() {
-  fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-    .then((responese) => responese.json())
+  fetch('https://27.javascript.pages.academy/kekstagram-simple/data', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then((responese) => {
+      if (responese.ok) {
+        return responese.json();
+      }
+    })
     .then((pictures) => {
       creatingTemplatePictures(pictures);
+    })
+    .catch(() => {
+      errorMessage();
     });
 }
 
